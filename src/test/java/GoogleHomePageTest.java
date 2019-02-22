f2397f756d9b931bcdbedf46e21566bc1f7508dd
Wojciech Puchalski <wojciech.puchalski9@wp.pl>
2019-02-22 18:09:37 +0100

    First working test, Webdriver Manager implementation

--- src/test/java/GoogleHomePageTest.java
+++ src/test/java/GoogleHomePageTest.java
@@ -1,20 +1,23 @@
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class GoogleHomePageTest {
 
     private WebDriver driver;
-    String appURL = "http://google.com";
 
     @BeforeClass
-    public void testSetUp() {
-
-        driver = new FirefoxDriver();
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
     }
+
+    String appURL = "http://google.com";
 
     @Test
     public void verifyGooglePageTittle() {
